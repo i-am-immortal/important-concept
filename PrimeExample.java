28a1e9ac666ae2f4c674da64803fdc7d4cce6e56
i-am-immortal <127956782+i-am-immortal@users.noreply.github.com>
2024-01-24 22:40:20 +0530

    Updated

--- PrimeExample.java
+++ PrimeExample.java
@@ -1,9 +1,7 @@
 public class PrimeExample {
     public static void main(String args[]) {
-
-        int n = 17;
-        int flag = 0;
-        if (n == 1 || n == 0) {
+        int n = 6, flag = 0;
+        if (n == 0 || n == 1) {
             System.out.println("number is not prime ");
         } else {
             for (int i = 2; i <= n; i++) {
@@ -11,12 +9,12 @@ public class PrimeExample {
                     System.out.println("number is not prime ");
                     flag = 1;
                     break;
+
                 }
                 if (flag == 0) {
                     System.out.println("number is prime ");
                 }
                 break;
-
             }
         }
     }
